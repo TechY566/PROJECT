2b2dc3080fb1c27ced922cbfdacd761a3de17d45
TechY566 <72405251+TechY566@users.noreply.github.com>
2020-10-17 22:58:28 +0100

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,23 @@
+class Paper
+{
+constructor(x,y,r)
+  {
+      var option ={
+    isStatic:false,
+    restitution:0.3
+    friction:0.5,
+    density:1.2
+
+
+
+
+      }
+
+
+
+this.x=x;
+this.y=y;
+this.r=r;
+
+
+
